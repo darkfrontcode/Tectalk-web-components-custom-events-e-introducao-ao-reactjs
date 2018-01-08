64563cd9e20dfffcf15d2ddf897366e17f97dd95
DarkFrontCode <darkfrontcode@gmail.com>
2018-01-08 13:40:33 -0200

    First Component of Project

--- source/index.ts
+++ source/index.ts
@@ -1,1 +1,38 @@
-console.log('hi')
+class buttonComponent extends HTMLElement
+{
+	// public name: string
+	// public shadow: ShadowRoot
+
+	constructor()
+	{
+		super()
+		// this.shadow = this.attachShadow({mode: 'open'})
+		// this.shadow.innerHTML = `
+		// 	<style>
+		// 		body
+		// 		{
+		// 			background: red;
+		// 		}
+		// 	</style>
+		// 	<div class="button-component">${this.text}</div>
+		// `
+	}
+
+	static get observedAttributes()
+	{
+		return ['name']
+	}
+
+	attributeChangedCallback(name:any, old_value:any, new_value:any)
+	{
+		switch(name)
+		{
+			case 'name': 
+				this.textContent = `Hello, ${new_value}`
+			break
+		}
+	}
+
+}
+
+customElements.define('button-component', buttonComponent);
