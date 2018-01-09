b99433727305891b1821d422c2eaae2e9a4fa28f
DarkFrontCode <darkfrontcode@gmail.com>
2018-01-09 14:58:58 -0200

    button component

--- source/index.ts
+++ source/index.ts
@@ -11,14 +11,50 @@ class buttonComponent extends HTMLElement
 
 	public connectedCallback() : void
 	{
-		this.shadow.innerHTML = `
+
+		const style = `
 			<style>
-				body
+				button
 				{
-					background: red;
+					background: #08C9D8;
+					font-family: 'Lobster', cursive;
+					font-size: 20px;
+					padding: 0 40px;
+					border: 0;
+					position: relative;
+					transition: background-color .25s;
+					display: flex;
+					flex-grow: 1;
+					flex-shrink: 0;
+					justify-content: center;
+					height: 55px;
+					color: white;
+					text-align: center;
+					margin: auto;
+					border-radius: 36px;
+					text-shadow: 0 2px 0 rgba(0,0,0,.2);
+					box-shadow: inset 0 -4px 0 0 rgba(0,0,0,.2), 0 4px 0 0 rgba(0,0,0,.2);
+					cursor: pointer;
+				}
+
+				button:focus
+				{
+					outline: none;
+				}
+
+				button:active
+				{
+					box-shadow: inset 0 -2px 0 0 rgba(0,0,0,.2), 0 2px 0 0 rgba(0,0,0,.2);
+					top: 2px;
 				}
 			</style>
-			<div class="button-component">${this.name}</div>
+		`
+
+		const template = `<button>${this.name}</button>`
+
+		this.shadow.innerHTML = `
+			${style}
+			${template}
 		`
 	}
 
