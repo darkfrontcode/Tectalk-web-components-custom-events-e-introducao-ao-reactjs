a27b7f85f34fc1a5a2539027a96cd43fef69474d
DarkFrontCode <darkfrontcode@gmail.com>
2018-01-10 13:56:36 -0200

    custom events

---
+++ source/utils/friend.class.ts
@@ -0,0 +1,9 @@
+export class Friend
+{
+	public name:string
+
+	constructor(name:string)
+	{
+		this.name = name;
+	}
+}
---
+++ source/utils/friends.list.ts
@@ -0,0 +1,16 @@
+import { Friend } from './friend.class'
+
+export const Friends = new Array<Friend>(
+	new Friend("Wesley Safadão"),
+	new Friend("Cpd. Washington"),
+	new Friend("Nego do Borel"),
+	new Friend("Sérgio Mallandro"),
+	new Friend("Gugu"),
+	new Friend("Latino"),
+	new Friend("Naldo Benny"),
+	new Friend("Abestado"),
+	new Friend("Cantor Belo"),
+	new Friend("Taylor Lautner"),
+	new Friend("Jim Carrey"),
+	new Friend("Nicolas Cage"),
+)
